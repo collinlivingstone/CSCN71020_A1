f81e762594c2747ce70ee1ccad24e02db127b64a
Collin Livingstone <Clivingstone1043@conestogac.on.ca>
2024-10-03 22:15:01 -0400

    Fixed the switch case that ran the add function more than once.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,7 +21,16 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+
+	case 2:
+		subtract();
+		break;
+
+	default:
+		break;
 	}
+	
 
 }
 
@@ -44,4 +54,14 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
